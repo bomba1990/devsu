5130ce4f0f6fd21353d0e9185d14b8ce90a22580
bomba1990 <marianoramirez353@gmail.com>
2017-10-28 02:43:11 -0400

    App initial

---
+++ app.py
@@ -0,0 +1,15 @@
+import json
+
+from flask import Flask
+
+# Connect to Redis
+app = Flask(__name__)
+
+
+@app.route("/hello/<name>")
+def hello(name):
+    return json.dumps({"message": f"Hello {name.capitalize()}"})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9001)
