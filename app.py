bb1acd3b7927ea710c9944860ce8c10467f9bfb5
bomba1990 <marianoramirez353@gmail.com>
2017-10-29 18:24:08 -0400

    test

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 
 @app.route("/hello/<name>")
 def hello(name):
-    return json.dumps({"message": f"Hello {name.capitalize()}aa"})
+    return json.dumps({"message": f"Hello {name.capitalize()}"})
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9001)
+
